05977588b3be1b70a320339cd815136ef50d9602
liuhuanyong <lhy_in_blcu@126.com>
2018-04-17 22:12:41 +0800

    Update keyinfo_extract.py

--- keyinfo_extract.py
+++ keyinfo_extract.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# File: keyinfo_nlp.py
+# File: keyinfo_extract.py
 # Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
 # Date: 18-4-17
 from script.keywords_textrank import *
